667e55ba55528537b11dd6a88b1f21a0dcc37182
xhf <xihengfei@jd.com>
2021-01-28 11:36:11 +0800

    init

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,15 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+console.log(1111111111111)
+@Options({
+  props: {
+    msg: String
+  }
+})
+export default class HelloWorld extends Vue {
+    msg!: string
+}
+</script>
